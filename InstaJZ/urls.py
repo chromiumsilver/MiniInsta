b1c2e1f320f2bfd52ca1fb32ec1da83371b96d9a
chromiumsilver <jiaying_yao@berkeley.edu>
2020-01-11 20:32:14 -0800

    version 0.1

---
+++ InstaJZ/urls.py
@@ -0,0 +1,28 @@
+"""Insta URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from InstaJZ.views import (HelloWorld, PostsView, PostCreateView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserDetailView, addLike)
+urlpatterns = [
+    path('helloworld', HelloWorld.as_view(), name='helloworld'),
+    path('', PostsView.as_view(), name='posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='make_post'),
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name='update_post'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='delete_post'),
+    path('like', addLike, name='addLike'),
+    path('user/<int:pk>/', UserDetailView.as_view(), name='profile'),
+]
---
+++ InstaJZ/views.py
@@ -0,0 +1,70 @@
+from annoying.decorators import ajax_request
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
+
+from InstaJZ.models import InstaUser, Post, Like
+from InstaJZ.forms import CustomUserCreationForm
+
+class HelloWorld(TemplateView):
+    template_name = 'test.html'
+
+class PostsView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+    def get_queryset(self):
+        current_user = self.request.user
+        following = set()
+        for conn in current_user.get_connections().select_related('following'):
+            following.add(conn.following)
+        
+        return Post.objects.filter(author__in=following)
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
+    login_url = 'login'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy("posts")
+
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy("login")
+
+class UserDetailView(DetailView):
+    model = InstaUser
+    template_name = 'user_profile.html'
+
+@ajax_request
+def addLike(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as _:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+    
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
